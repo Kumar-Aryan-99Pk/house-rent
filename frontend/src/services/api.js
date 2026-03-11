4e02dfad87ebeed9091490319d46230acbd66ddc
Kumar Aryan <karyan99gk@gmail.com>
2026-03-11 23:19:11 +0600

    updated api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://househunt-backend.onrender.com/api"
+  baseURL: "https://homehunt-backend.onrender.com/api"
 });
 
 export default API;
